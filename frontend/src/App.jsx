2a9c53388e25e977a47736dbb077a980b61f1a33
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-07-26 10:57:34 +0200

    show errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,18 @@ import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/items`)
-      .then((response) => setItems(response.data));
+      .then((response) => setItems(response.data))
+      .catch((err) => setError(JSON.stringify(err)));
   }, []);
 
   return (
     <div className="App">
+      {error && <pre>{error}</pre>}
       <ul>
         {items.map((item) => (
           <li key={item.id}>{item.title}</li>
